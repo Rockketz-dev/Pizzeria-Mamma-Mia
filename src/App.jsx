5addf892b9d407660b06d696598492ab8c717704
Felipe Osorio B <f.osorio@outlook.com>
2025-01-19 20:39:40 -0300

    hito 2 React

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Register from './components/Register'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/*<Register />*/}
+      <Login />
       <Footer />
     </>
   )
