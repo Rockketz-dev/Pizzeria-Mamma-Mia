922977ff6cba32fb964d01a7e01d8d7d86cf1004
Felipe Osorio B <f.osorio@outlook.com>
2025-01-26 23:19:20 -0300

    git commit -m "Add Cart component y CardPizza component - Hito 3"

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,15 +1,27 @@
 import '../components/CardPizza.css'
 
-const CardPizza = ({ name, price, ingredients, img }) => {
+const CardPizza = ({ desc, id, img, ingredients, name, price }) => {
+  const capitalize = text => {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  }
+
   return (
     <div className="card mt-5 mb-5">
       <img src={img} alt={`Pizza ${name}`} className="card-img" />
-      <h4 className="text-start fw-semibold p-2">Pizza {name}</h4>
+      <h4 className="text-start text-capitalize display-6 p-1">
+        Pizza {capitalize(name)}
+      </h4>
       <hr />
-      <p className="lead text-secondary-emphasis">Ingredientes:</p>
-      <p>
-        <small>🍕 {ingredients.join(', ')}</small>
-      </p>
+      <p className="lh-1 fs-6">{desc}</p>
+      <hr />
+      <h5 className="text-start p-2">Ingredientes:</h5>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index} className="text-start">
+            {capitalize(ingredient)}
+          </li>
+        ))}
+      </ul>
       <hr />
       <h5>Precio: ${price.toLocaleString('es-CL')}</h5>
 
